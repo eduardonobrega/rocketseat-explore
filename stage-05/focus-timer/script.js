3a3cda8fa76fe56c19af304c821d06229d3005b2
Eduardo Nóbrega <87456011+eduardonobrega@users.noreply.github.com>
2022-07-21 13:47:46 -0300

    style

---
+++ stage-05/focus-timer/script.js
@@ -0,0 +1,12 @@
+console.log('Hello World');
+
+let me = {
+  name: 'Eduardo',
+  age: 23,
+  occupation: 'Full Stack Developer',
+  adm: 'Sarah',
+
+  action: () => {
+    console.log('Code your life')
+  }
+};
